e992fe8a694b75765329f7de562443a7924d8e77
Diego Ramos <diegorbrito9@gmail.com>
2025-02-07 15:27:00 -0300

    [2 section] [fundamentals] end of class 'Appollo Server'

--- section-2-fundamentals/server/src/server.ts
+++ section-2-fundamentals/server/src/server.ts
@@ -1,3 +1,6 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
 const typeDefs = `#graphql
   type Query {
     greeting: String
@@ -9,3 +12,10 @@ const resolvers = {
     greeting: () => "Hello GraphQL!",
   },
 };
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀 Server ready at ${url}`);
