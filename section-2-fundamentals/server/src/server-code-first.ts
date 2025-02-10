153e054e19d11222ccfe6474dfc75a068067b69b
Diego Ramos <diegorbrito9@gmail.com>
2025-02-10 18:39:14 -0300

    [2 section] [fundamentals] end of class 'code first vs schema-first'

---
+++ section-2-fundamentals/server/src/server-code-first.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { makeSchema, queryType } from "nexus";
+
+const Query = queryType({
+  definition(t) {
+    t.string("greeting", {
+      resolve: () => "Hello GraphQL!",
+    });
+  },
+});
+
+const schema = makeSchema({
+  types: [Query],
+});
+
+const server = new ApolloServer({ schema });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+console.log(`🚀 Server ready at ${url}`);
