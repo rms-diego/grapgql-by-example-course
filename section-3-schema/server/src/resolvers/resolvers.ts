fec772e2a51a9e55460b391985047c39c8e446cd
Diego Ramos <diegorbrito9@gmail.com>
2025-03-23 18:53:57 -0300

    [3 section] [schema] end of class 'object associaions'

--- section-3-schema/server/src/resolvers/resolvers.ts
+++ section-3-schema/server/src/resolvers/resolvers.ts
@@ -1,8 +1,10 @@
+import { getCompany } from "../db/companies.js";
 import { getJobs } from "../db/jobs.js";
 
 export const resolvers = {
   Job: {
     date: (job: Job) => job.createdAt.split("T")[0],
+    company: (job: Job) => getCompany(job.companyId),
   },
 
   Query: {
