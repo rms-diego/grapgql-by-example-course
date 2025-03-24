5391b1420782c0f6e2b3084dbcf2abe64ad77b6c
Diego Ramos <diegorbrito9@gmail.com>
2025-03-23 21:43:45 -0300

    [5 section] [errors] end of class 'custom errors'

--- section-5-errors/server/src/resolvers/resolvers.ts
+++ section-5-errors/server/src/resolvers/resolvers.ts
@@ -1,6 +1,8 @@
 import { getCompany } from "../db/companies.js";
 import { getJob, getJobs, getJobsByCompany } from "../db/jobs.js";
 
+import { notFoundError } from "../utils/not-found-error.js";
+
 export const resolvers = {
   Job: {
     date: (job: Job) => job.createdAt.split("T")[0],
@@ -14,7 +16,21 @@ export const resolvers = {
   Query: {
     hello: () => "Hello world!",
     jobs: () => getJobs(),
-    job: (_parent, { id }: { id: string }) => getJob(id),
-    company: (_parent, { id }: { id: string }) => getCompany(id),
+    job: async (_parent, { id }: { id: string }) => {
+      const job = await getJob(id);
+      if (!job) {
+        throw notFoundError("Job not found");
+      }
+
+      return job;
+    },
+    company: async (_parent, { id }: { id: string }) => {
+      const company = await getCompany(id);
+      if (!company) {
+        throw notFoundError("Company not found");
+      }
+
+      return company;
+    },
   },
 };
